b3286f1c043622baee1b7376f2630281839f52dc
James Henry <jhenry@vigosoftware.com>
2023-09-04 13:52:50 +0100

    update

--- cslox/Lox.cs
+++ cslox/Lox.cs
@@ -4,6 +4,44 @@ class Lox
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length > 1)
+        {
+            Console.WriteLine("Usage: cslox [script]");
+            Environment.Exit(64);
+        }
+        else if (args.Length == 1)
+        {
+            runFile(args[0]);
+        }
+        else
+        {
+            runPrompt();
+        }
+    }
+
+    private static void runPrompt()
+    {
+        StreamReader reader = new(Console.OpenStandardInput());
+
+        for (; ; )
+        {
+            Console.Out.WriteLine("> ");
+            string? line = reader.ReadLine();
+            if (line == null) break;
+            run(line);
+        }
+    }
+
+    private static void runFile(string path)
+    {
+        byte[] bytes = File.ReadAllBytes(path);
+        run(System.Text.Encoding.Default.GetString(bytes));
+    }
+
+
+
+    private static void run(string line)
+    {
+
     }
 }
